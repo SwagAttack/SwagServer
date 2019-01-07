e8db233aee0f4a6754ded393d21df41ef805585a
Jonas Andersen <jonasna93@gmail.com>
2019-01-07 16:02:19 +0100

    updated comments

--- Container/src/constants/metadataKeys.ts
+++ Container/src/constants/metadataKeys.ts
@@ -1,12 +1,12 @@
 export const enum MetadataKeys {
 
     /**
-     * Key describing metadata which holds binding information for a single binding
+     * Key describing metadata which holds binding information for a single injection
      */
     Inject = "swagjs:inject",
 
     /**
-     * Key describing metadata which holds binding information for a multi binding
+     * Key describing metadata which holds binding information for a multi injection
      */
     MultiInject = "swagjs:multiinject",
 
